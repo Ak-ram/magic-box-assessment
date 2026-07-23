064ae0db228117215b943f4bddb6d8485e073071
Akram Ashraf <ak80246@banquemisr.com>
2026-07-23 15:14:35 +0300

    create header component

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   standalone: true,
 })
-export default class LoginComponent {}
+export default class LoginComponent {
+  // State
+  email = signal('');
+  password = signal('');
+  rememberMe = signal(false);
+  showPassword = signal(false);
+
+  // Methods
+
+  // Add description here also
+  togglePassword() {
+    this.showPassword.update((v) => !v);
+  }
+  // here description needed
+  submit() {}
+}
