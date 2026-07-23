434289ffea0d31f6596066368406a2b1e276e23a
Akram Ashraf <ak80246@banquemisr.com>
2026-07-23 17:05:42 +0300

    create init purchase component

--- src/pages/purchase/purchase.component.ts
+++ src/pages/purchase/purchase.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SummaryCardComponent } from '../../components';
 @Component({
   selector: 'purchase',
   templateUrl: './purchase.component.html',
   standalone: true,
+  imports: [SummaryCardComponent],
 })
-export default class PurchaseComponent {}
+export default class PurchaseComponent {
+  // State
+  name = signal('');
+  mobile = signal('');
+  childName = signal('');
+  childDob = signal('');
+  gender = signal<'boy' | 'girl'>('boy');
+  price = signal(456);
+  taxes = signal(4.0);
+
+  // Methods
+
+  // Note: Add jsDoc description here
+  addChild() {}
+
+  // Note: Add jsDoc description here
+  submit() {}
+}
