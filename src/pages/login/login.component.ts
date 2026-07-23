6f7d3e47e372765c423ea7e7a7d9ac7e295916ea
Akram Ashraf <ak80246@banquemisr.com>
2026-07-24 01:32:26 +0300

    update login form component

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,22 +1,36 @@
 import { Component, signal } from '@angular/core';
+import { ILoginForm } from '../../models/auth.model';
+import { form, FormField, required, email, minLength } from '@angular/forms/signals';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   standalone: true,
+  imports: [FormField],
 })
 export default class LoginComponent {
   // State
-  email = signal('');
-  password = signal('');
-  rememberMe = signal(false);
-  showPassword = signal(false);
+  rememberMe = signal<boolean>(false);
+  loginModal = signal<ILoginForm>({
+    email: '',
+    password: '',
+  });
+  loginForm = form(this.loginModal, (schemaPath) => {
+    required(schemaPath.email, { message: 'Email is required' });
+    email(schemaPath.email, { message: 'Email is not valid' });
+    required(schemaPath.password, { message: 'Password is required' });
+    minLength(schemaPath.password, 6, { message: 'Password must be at least 6 characters' });
+  });
 
   // Methods
-
-  // Add description here also
-  togglePassword() {
-    this.showPassword.update((v) => !v);
+  submit() {
+    if (this.loginForm().valid()) {
+      console.log('Form Data:', this.loginModal());
+      console.log('Remember Me:', this.rememberMe());
+    }
   }
-  // here description needed
-  submit() {}
+
+  onRememberMeChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.rememberMe.set(input.checked);
+  }
 }
