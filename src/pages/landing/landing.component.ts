a5eed105b256f4789cc2b4d104f837298008bd95
Akram Ashraf <ak80246@banquemisr.com>
2026-07-24 01:31:56 +0300

    add auth service

--- src/pages/landing/landing.component.ts
+++ src/pages/landing/landing.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'landing',
   templateUrl: './landing.component.html',
   standalone: true,
 })
-export default class LandingComponent {}
+export default class LandingComponent implements OnInit {
+  // injection list
+  private readonly authService: AuthService = inject(AuthService);
+  private readonly router: Router = inject(Router);
+
+  ngOnInit() {
+    this.authService.login().subscribe(res=>{
+      console.log('auth', res);
+      setTimeout(()=>{
+      this.authService.getUserProfile(res.accessToken).subscribe((res) => console.log(res));
+      }, 2000)
+    });
+
+  }
+
+  goToPurchase() {
+    this.router.navigate(['/purchase']);
+  }
+}
---
+++ src/services/auth.service.ts
@@ -0,0 +1,57 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { loginResponse, userProfileResponse } from '../models/auth.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  loginUrl = 'https://dummyjson.com/auth/login';
+  userProfileUrl = 'https://dummyjson.com/auth/me';
+  refreshTokenUrl = 'https://dummyjson.com/auth/refresh';
+  // Injection list
+  private readonly http: HttpClient = inject(HttpClient);
+
+  login(): Observable<loginResponse> {
+    const body = JSON.stringify({
+      username: 'emilys',
+      password: 'emilyspass',
+      expiresInMins: 30,
+    });
+    return this.http
+      .post(this.loginUrl, body, {
+        headers: {
+          'Content-Type': 'application/json',
+          credentials: 'include',
+        },
+      })
+      .pipe(map((res: Object) => res as loginResponse));
+  }
+
+  getUserProfile(accessToken: string): Observable<Object> {
+    return this.http
+      .get(this.userProfileUrl, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          credentials: 'include',
+        },
+      })
+      .pipe(map((res: Object) => res as userProfileResponse));
+  }
+
+  refreshToken(refreshToken: string): Observable<Object> {
+    const body = JSON.stringify({
+      refreshToken: '/* YOUR_REFRESH_TOKEN_HERE */',
+      expiresInMins: 30,
+    });
+    return this.http
+      .post(this.refreshTokenUrl, body, {
+        headers: {
+          'Content-Type': 'application/json',
+          credentials: 'include',
+        },
+      })
+      .pipe(map((res: Object) => res as loginResponse));
+  }
+}
