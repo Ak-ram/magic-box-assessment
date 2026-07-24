1d2254a468fbaa0d2ff379dd0e096e6f37a4d97c
Akram Ashraf <ak80246@banquemisr.com>
2026-07-24 11:12:10 +0300

    update header component

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   standalone: true,
+  imports: [RouterLink],
 })
+export default class HeaderComponent {
+  // Injection list
+  private readonly authService: AuthService = inject(AuthService);
+  private readonly router: Router = inject(Router);
 
-export default class HeaderComponent {}
+  // State
+  isAuthenticated = this.authService.isAuthenticated;
+
+  // Methods
+  // Note: Add jsDoc description here
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/landing']);
+  }
+}
