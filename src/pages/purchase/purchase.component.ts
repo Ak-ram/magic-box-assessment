b8bb8e66f2a7141549cb216a6fa98161e1cb6327
Akram Ashraf <ak80246@banquemisr.com>
2026-07-24 11:05:49 +0300

    update

--- src/pages/purchase/purchase.component.ts
+++ src/pages/purchase/purchase.component.ts
@@ -1,6 +1,20 @@
-import { Component, inject, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { SummaryCardComponent } from '../../components';
-import { form, FormField, required, email, minLength, maxLength, pattern } from '@angular/forms/signals';
+import {
+  form,
+  FormField,
+  required,
+  maxLength,
+  applyEach,
+  validate,
+  pattern,
+} from '@angular/forms/signals';
 import { IPurchaseForm } from '../../models/auth.model';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
@@ -18,16 +32,34 @@ export default class PurchaseComponent {
   private readonly router = inject(Router);
 
   // State
+  childrenWrapper = viewChild<ElementRef<HTMLDivElement>>('childrenWrapper');
   purchaseModel = signal<IPurchaseForm>(
-    this.purchaseService.draft() ?? { name: '', mobile: '', children: [
-      { childFullName: '', childDOB: '', gender: 'boy' },
-      ] },
+    this.purchaseService.draft() ?? {
+      name: '',
+      mobile: '',
+      children: [{ childFullName: '', childDOB: '', gender: 'boy' }],
+    },
   );
   purchaseForm = form(this.purchaseModel, (schemaPath) => {
     required(schemaPath.name, { message: 'Name is required' });
     required(schemaPath.mobile, { message: 'Mobile is required' });
-    // maxLength(schemaPath.mobile, 11, { message: 'Mobile must be at most 11 characters' });
-    pattern(schemaPath.mobile, /^01\d{9}$/, { message: 'Mobile must be 11 digits starting with 01' });
+    maxLength(schemaPath.mobile, 11, { message: 'Mobile must be 11 digits' });
+    pattern(schemaPath.mobile, /^01\d{9}$/, {
+      message: 'Mobile must be 11 digits starting with 01',
+    });
+    applyEach(schemaPath.children, (child) => {
+      required(child.childFullName, { message: 'Child name is required' });
+      required(child.childDOB, { message: 'Child date of birth is required' });
+
+      validate(child.childDOB, ({ value }) => {
+        const dob = new Date(value());
+        const sevenYearsAgo = new Date();
+        sevenYearsAgo.setFullYear(sevenYearsAgo.getFullYear() - 7);
+        return dob <= sevenYearsAgo
+          ? null
+          : { kind: 'minAge', message: 'Child must be at least 7 years old' };
+      });
+    });
   });
   price = signal(456);
   taxes = signal(4.0);
