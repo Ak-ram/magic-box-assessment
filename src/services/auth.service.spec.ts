5384e74a85961893caccf49535767a091330e8a3
Akram Ashraf <ak80246@banquemisr.com>
2026-07-25 12:35:48 +0300

    handle auth.service.spec.ts file

--- src/services/auth.service.spec.ts
+++ src/services/auth.service.spec.ts
@@ -27,4 +27,42 @@ describe('AuthService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should start as not authenticated', () => {
+    expect(service.isAuthenticated()).toBe(false);
+  });
+
+  it('should store access token in session-storage when rememberMe is false', () => {
+    service.login({ username: 'emilys', password: 'emilyspass' }, false).subscribe();
+
+    const req = httpMock.expectOne('https://dummyjson.com/auth/login');
+    expect(req.request.method).toBe('POST');
+    req.flush({ accessToken: 'test-access-token' });
+
+    expect(service.isAuthenticated()).toBe(true);
+    expect(sessionStorage.getItem('accessToken')).toBe('test-access-token');
+    expect(localStorage.getItem('accessToken')).toBeNull();
+  });
+
+  it('should store access token in local storage when rememberMe is true', () => {
+    service.login({ username: 'emilys', password: 'emilyspass' }, true).subscribe();
+    const req = httpMock.expectOne('https://dummyjson.com/auth/login');
+    req.flush({ accessToken: 'test-access-token' });
+    expect(localStorage.getItem('accessToken')).toBe('test-access-token');
+    expect(sessionStorage.getItem('accessToken')).toBeNull();
+  });
+
+  it('should clear both storages (session, locale) when logout is called', () => {
+    service.login({ username: 'emilys', password: 'emilyspass' }, true).subscribe();
+    const req = httpMock.expectOne('https://dummyjson.com/auth/login');
+    req.flush({ accessToken: 'test-access-token' });
+
+    expect(service.isAuthenticated()).toBe(true);
+
+    service.logout();
+
+    expect(service.isAuthenticated()).toBe(false);
+    expect(localStorage.getItem('accessToken')).toBeNull();
+    expect(sessionStorage.getItem('accessToken')).toBeNull();
+  });
 });
